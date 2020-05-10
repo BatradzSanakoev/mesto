dc4abb8659644275929effff898344f805e2e2e9
Bato <sanakoev.batraz@yandex.ru>
2020-05-10 05:33:32 +0300

    fix: исправить все ошибки

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-  let content = document.querySelector('.content'),
+  const content = document.querySelector('.content'),
     edit = content.querySelector('.profile__edit-button'),
     pop = document.querySelector('.pop-up'),
     close = document.querySelector('.pop-up__close-button'),
@@ -7,26 +7,19 @@ window.onload = function () {
     popDesc = document.querySelector('.pop-up__input_desc'),
     profName = content.querySelector('.profile__name'),
     profDesc = content.querySelector('.profile__description'),
-    form = document.querySelector('.pop-up__form'),
-    editHoverIcon = content.querySelector('.profile__edit-icon'),
-    editHoverButton = content.querySelector('.profile__edit-button'),
-    addHoverIcon = content.querySelector('.profile__add-icon'),
-    addHoverButton = content.querySelector('.profile__add-button'),
-    body = document.querySelector('.root');
+    form = document.querySelector('.pop-up__form');
 
   function editButton() {
     if (pop.classList.contains('pop-up_opened')) {
       pop.classList.remove('pop-up_opened');
       popName.value = profName.textContent;
       popDesc.value = profDesc.textContent;
-      body.setAttribute('style', 'overflow: hidden;');
     }
   }
 
   function closeButton() {
     if (!pop.classList.contains('pop-up_opened')) {
       pop.classList.add('pop-up_opened');
-      body.removeAttribute('style');
     }
   }
 
@@ -36,26 +29,10 @@ window.onload = function () {
     profName.textContent = popName.value;
     profDesc.textContent = popDesc.value;
     pop.classList.toggle('pop-up_opened');
-    body.removeAttribute('style');
   }
 
   edit.addEventListener('click', editButton);
   close.addEventListener('click', closeButton);
   form.addEventListener('submit', formSubmitHandler);
 
-  editHoverButton.onmouseover = function() {
-    editHoverIcon.setAttribute('style', 'opacity: .6;');
-  }
-
-  editHoverButton.onmouseout = function() {
-    editHoverIcon.removeAttribute('style');
-  }
-
-  addHoverButton.onmouseover = function() {
-    addHoverIcon.setAttribute('style', 'opacity: .6;');
-  }
-
-  addHoverButton.onmouseout = function() {
-    addHoverIcon.removeAttribute('style');
-  }
 }
